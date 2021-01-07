72e9a4acae6d20be95e7794a0440a1274fdf75aa
matthew colson <matthew.p.colson@gmail.com>
2021-01-07 14:24:13 -0600

    updated app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,99 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage(
+                  'images/snoopy.jpg',
+                ),
+              ),
+              Text(
+                'Snoopy',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'COMIC CHARACTER',
+                style: TextStyle(
+                  color: Colors.red.shade100,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.0,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.red.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    '123-456-7890',
+                    style: TextStyle(
+                      color: Colors.grey.shade700,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.red,
+                  ),
+                  title: Text(
+                    'snoopy@peanuts.com',
+                    style: TextStyle(
+                      color: Colors.grey.shade700,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
